0f0c5bb71805604cacabc2e1cc1adc52fc713595
DevInCube <ruslan.hadyniak@gmail.com>
2015-08-19 18:57:00 +0300

    Create main.c

---
+++ tests/types/monofunction/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ( int argc, char *argv[] )
+{
+  if ( argc != 2 ) /* argc should be 2 for correct execution */
+  {
+    return 1;
+  }
+  else 
+  {
+    int num_tests = argv[1];
+    while(num_tests-- >= 0)
+    {
+      // 
+    }
+    return 0;
+  }
+}
